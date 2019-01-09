db1839f844b2e7d28b5db8ec6c0e7dbf261fb210
Obare <obaregeoffrey78@gmail.com>
2019-01-09 22:38:35 +0300

    All working script

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const admin = require("firebase-admin")
+const serviceAccount = require("./serviceAccount.json")
+const data = require("./realtimeData.json")
+const dbUrl = require("./dbUrl.js")
+const coll = process.argv[2]
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: dbUrl
+})
+
+const db = admin.firestore()
+//Ariga
+Object.keys(data).forEach(it => {
+  const item = data[it]
+  db.collection(coll).add(item)
+})
